152420b0b43868d3cc056851ba76aec6f426def5
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-04-12 07:17:38 +0200

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React, { useState } from "react";
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
-    const [loading, setLoading] = useState(true);
-    const [tours, setTours] = useState(true);
-        <h1>Hello world</h1>
+    <main>
+      <Tours />
+    </main>
   );
 }
 
